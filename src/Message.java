3a341dabeac972527ffabeaf9dbd240d64b4171b
unknown <e1800917@edu.vamk.fi>
2020-09-20 23:10:34 +0300

    refactor

--- src/Message.java
+++ src/Message.java
@@ -1,13 +1,16 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Message {
 	private String name, message;
 	private Date date;
+	SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+	SimpleDateFormat dateFormat1 = new SimpleDateFormat("MM/dd/yyyy");
 
-	public Message(String name, String message,Date date) {
+	public Message(String name, String message, Date date) {
 		this.name = name;
 		this.message = message;
-		this.date=date;
+		this.date = date;
 	}
 
 	public Date getDate() {
@@ -34,4 +37,101 @@ public class Message {
 		this.message = message;
 	}
 
+	public String printInformation() {
+		String result = "<div";
+		result += " class=\"card m-2 \"  style=\"max-width:18rem\">";
+		result += "<h5 class=\"card-header bg-info text-white\">" + name + "</h5>";
+		result += "<div class=\"card-body\"><p class=\"card-text\">" + "  " + message + "</p>";
+		result += "<p class=\"card-text\"><small class=\"text-muted\">" + dateFormat.format(date) + "</small></p>\r\n"
+				+ "</div>";
+		result += "</div>";
+		return result;
+	}
+
+	public String printName(Message item) {
+		String result = "<div";
+		result += " class=\"card m-2 \"  style=\"max-width:18rem\">";
+		result += "<h5 class=\"card-header bg-info text-white\">" + item.getName() + "</h5>";
+		result += "<div class=\"card-body\"><p class=\"card-text\">" + "  " + item.getMessage() + "</p>";
+		result += "<p class=\"card-text\"><small class=\"text-muted\">" + dateFormat.format(item.getDate())
+				+ "</small></p>\r\n" + "</div>";
+		result += "</div>";
+
+		return result;
+	}
+
+	public String printDate(Message item) {
+		String result = "<div";
+		result += " class=\"card m-2 \"  style=\"width:40%\">";
+		result += "<div class=\"card-body\"><p class=\"card-text\">" + "  " + item.getMessage() + "</p>";
+		result += "<p class=\"card-text\"><small class=\"text-muted\">" + "by " + item.getName() + " on "
+				+ dateFormat1.format(item.getDate()) + "</small></p>\r\n" + "</div>";
+		result += "</div>";
+
+		return result;
+	}
+
+	public static String printError() {
+		String result = "<script ";
+		result += "type=\"text/javascript\">";
+		result += "alert('Incorrect values');";
+
+		result += "</script>";
+
+		return result;
+	}
+	
+	public static String printHtmlLayoutOfSearching() {
+		String result="<!DOCTYPE html>\r\n" + "<html>\r\n" + "<head>\r\n";
+		result+= "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\" integrity=\"sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z\" crossorigin=\"anonymous\">\r\n"
+				+ "" + "<meta charset=\"ISO-8859-1\">\r\n" + "<title>Search</title>\r\n"+" <link href=\"http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css\" rel=\"stylesheet\" type=\"text/css\"/>\r\n" + 
+						"  <script type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js\"></script>\r\n" + 
+						"  <script src=\"http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js\"></script>\r\n" + 
+						"  <script>\r\n" + 
+						"    $(document).ready(function() {\r\n" + 
+						"     $(\"#datepicker\").datepicker({\r\n" + 
+						"       changeMonth: true, \r\n" + 
+						"       changeYear: true\r\n" + 
+						"       });\r\n" + 
+						"    });\r\n" + 
+						"  </script>" + "</head>\r\n";
+		result+="<body>";
+		result+="<div class=\"d-flex justify-content-between p-2 bg-light mb-3 \"> <a href=\"search.html\">Back</a></div>\r\n" + 
+		" <form  action=\"search_servlet\" method=\"POST\">\r\n" + 
+		"   <div class=\"d-flex flex-row pt-5 justify-content-center align-items-center\" >\r\n" + 
+		"     <input style=\"width:30%\"  class=\"mr-3\" type=\"text\" name=\"name\" placeholder=\"Search name\" />\r\n" + 
+		"     \r\n" + 
+		"      <input   type=\"submit\" name=\"search\" value=\"Search Name\" />\r\n" + 
+		"   </div> <div class=\"d-flex flex-row pt-5 justify-content-center align-items-center\" >\r\n" + 
+		"   <input style=\"width:30%\" id=\"datepicker\" class=\"mr-3\" type=\"text\" name=\"date\" placeholder=\"DD/MM/YYYY\" />\r\n" + 
+		"   <input name=\"search\" type=\"submit\" value=\"Search Date\" />\r\n" + 
+		"  </div>\r\n" + 
+		"  </form>";
+		return result;
+	}
+	
+	public static String printHtmlLayoutOfSubmission(){
+		String result="<!DOCTYPE html>\r\n" + "<html>\r\n" + "<head>\r\n"
+				+ "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\" integrity=\"sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z\" crossorigin=\"anonymous\">\r\n"
+				+ "" + "<meta charset=\"ISO-8859-1\">\r\n" + "<title>Forum</title>\r\n" + "</head>\r\n";
+
+		result+="<body>";
+		result+="<div class=\"d-flex justify-content-between p-2 bg-light mb-3 \"><span class=\"text-black-50\">1800917</span><a class=\"\" href=\"search.html\">Search</a></div>";
+  		
+
+		result+="<form  action=\"submit_servlet\" method=\"POST\">\r\n" + 
+				"   <div class=\"d-flex flex-column pt-5 justify-content-center align-items-center\" >\r\n" + 
+				"     <input autofocus class=\"mb-2\" type=\"text\" name=\"name\" placeholder=\"Name\" />\r\n" + 
+				"      <textarea class=\"mb-3\"  rows=\"5\" cols=\"22\" name=\"message\" placeholder=\"Message\"></textarea>\r\n" + 
+				"      <input   type=\"submit\" value=\"Submit\" />\r\n" + 
+				"   </div>\r\n" + 
+				"     \r\n" + 
+				"    \r\n" + 
+				"   \r\n" + 
+				"     \r\n" + 
+				"    </form>";
+		result+="<div class=\"d-flex flex-row p-2 justify-content-start align-items-center \">";
+		
+		return result;
+	}
 }
--- src/SearchServlet.java
+++ src/SearchServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 /**
  * Servlet implementation class SearchServlet
  */
@@ -18,152 +19,90 @@ public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-	//Db database = new Db();
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-	private Vector<Message> messagesList=new Vector();
-	
-	
-//	public Vector<Message> getMessagesList() {
-//		return messagesList;
-//	}
-//	public void setMessagesList(Vector<Message> messagesList) {
-//		this.messagesList = messagesList;
-//	}
-	//Vector<Message> messagesList=database.getMessageDatabase();
-    public SearchServlet() {
-        super();
- 
-        // TODO Auto-generated constructor stub
-    }
 
-	/*
-	 * public Vector<Message> nameSearch(String name,Vector<Message> list){
-	 * Vector<Message> result=null; for(Message item:list) {
-	 * if(item.getName().toLowerCase().contains(name.toLowerCase())) {
-	 * result.addElement(item); } } return result; }
-	 */
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	int flag = 0;
+
+	public SearchServlet() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//HttpSession session=request.getSession();
-		//Vector<Message> messagesList = (Vector<Message>) session.getAttribute("messagesList");
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		
+		//String searchFilter = request.getParameterValues("search")!=null?request.getParameterValues("search")[0]:"";
 		
-		
-		//String searchFilter=request.getParameter(arg0)
-		//String searchFilter=request.getParameterValues("search")[0];
-		
-		
-		 // System.out.println(searchOptions.length);
-		 // String searchFilter=""; 
-		/*  if(item.equals("Search Name")) { 
-			  searchFilter=item; }
-		  else if(item.equals("Search Date")) { searchFilter=item; } } 
-		  for(String item:searchOptions) { System.out.println("ahihi "+item); }
-		  System.out.println(searchFilter);*/
-		 
+
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
+		String searchFilter = request.getParameterValues("search")[0];
 
-		out.println("<!DOCTYPE html>\r\n" + "<html>\r\n" + "<head>\r\n"
-				+ "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\" integrity=\"sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z\" crossorigin=\"anonymous\">\r\n"
-				+ "" + "<meta charset=\"ISO-8859-1\">\r\n" + "<title>Search</title>\r\n"+" <link href=\"http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css\" rel=\"stylesheet\" type=\"text/css\"/>\r\n" + 
-						"  <script type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js\"></script>\r\n" + 
-						"  <script src=\"http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js\"></script>\r\n" + 
-						"  <script>\r\n" + 
-						"    $(document).ready(function() {\r\n" + 
-						"     $(\"#datepicker\").datepicker({\r\n" + 
-						"       changeMonth: true, \r\n" + 
-						"       changeYear: true\r\n" + 
-						"       });\r\n" + 
-						"    });\r\n" + 
-						"  </script>" + "</head>\r\n");
-		out.print("<body>");
-		out.print("<div class=\"d-flex justify-content-between p-2 bg-light mb-3 \"> <a href=\"search.html\">Back</a></div>\r\n" + 
-		" <form  action=\"search_servlet\" method=\"POST\">\r\n" + 
-		"   <div class=\"d-flex flex-row pt-5 justify-content-center align-items-center\" >\r\n" + 
-		"     <input style=\"width:30%\"  class=\"mr-3\" type=\"text\" name=\"name\" placeholder=\"Search name\" />\r\n" + 
-		"     \r\n" + 
-		"      <input   type=\"submit\" name=\"search\" value=\"Search Name\" />\r\n" + 
-		"   </div> <div class=\"d-flex flex-row pt-5 justify-content-center align-items-center\" >\r\n" + 
-		"   <input style=\"width:30%\" id=\"datepicker\" class=\"mr-3\" type=\"text\" name=\"date\" placeholder=\"DD/MM/YYYY\" />\r\n" + 
-		"   <input name=\"search\" type=\"submit\" value=\"Search Date\" />\r\n" + 
-		"  </div>\r\n" + 
-		"  </form>");
+		out.print(Message.printHtmlLayoutOfSearching());
+		System.out.println(searchFilter);
 
-		String[] searchOptions=request.getParameterValues("search"); 
-		List<String> nameResult=null;
-		for(String searchFilter:searchOptions) {
-			System.out.println(searchOptions[0]+" ");
-			if (searchFilter.equals("Search Name")) {
-			System.out.println("message list "+messagesList);
+		if (searchFilter.equals("Search Name")) {
+
 			String name = request.getParameter("name");
-			out.print("<div class=\"d-flex flex-row p-2 justify-content-start align-items-center \">");
-			for(Message item:Db.getMessageDatabase()) {/*messagesList*/
-				System.out.println("ori name " + item.getName().toLowerCase());
-				System.out.println(name.toLowerCase());
-//				||(item.getName().toLowerCase().equalsIgnoreCase(name.toLowerCase())
-				if((item.getName().toLowerCase().equals(name.toLowerCase()))) {
-				//	nameResult.add(item.getName());
-					out.print("<div class=\"card m-2 \"  style=\"max-width:18rem\">");
-	    			out.print("<h5 class=\"card-header bg-info text-white\">" + item.getName() + "</h5>");
-	    			out.print("<div class=\"card-body\"><p class=\"card-text\">" + "  " + item.getMessage() +  "</p>");
-	    			out.print("<p class=\"card-text\"><small class=\"text-muted\">"+dateFormat.format(item.getDate())+"</small></p>\r\n"+ "</div>");
-	    			out.print("</div>");
-	    			break;
-				} 
-				else if((item.getName().toLowerCase().contains(name.toLowerCase()))) {
-					//nameResult.add(item.getName());
-	    			out.print("<div class=\"card m-2 \"  style=\"max-width:18rem\">");
-	    			out.print("<h5 class=\"card-header bg-info text-white\">" + item.getName() + "</h5>");
-	    			out.print("<div class=\"card-body\"><p class=\"card-text\">" + "  " + item.getMessage() +  "</p>");
-	    			out.print("<p class=\"card-text\"><small class=\"text-muted\">"+dateFormat.format(item.getDate())+"</small></p>\r\n"+ "</div>");
-	    			out.print("</div>");
-				}     
-		
-	    	}
 			
-			/*
-			 * if (!nameResult.contains(name)) {
-			 * out.println("<script type=\"text/javascript\">");
-			 * out.println("alert('Incorrect values');"); out.println("</script>"); }
-			 */
-		
-			out.print("</div>");
-			/*
-			 * out.print("</body>"); out.print("</html>"); out.close();
-			 */
-		}else {
-			String date = request.getParameter("date");
-			out.print("<div class=\"d-flex flex-column p-5 justify-content-center align-items-center \">");
-			for(Message item:/*messagesList*/Db.getMessageDatabase()) {
-				System.out.println(dateFormat.format(item.getDate()).toString());
-	    		if(dateFormat.format(item.getDate()).toString().equals(date)) {
-	    			out.print("<div class=\"card m-2 \"  style=\"width:40%\">");
-	    			out.print("<div class=\"card-body\"><p class=\"card-text\">" + "  " + item.getMessage() +  "</p>");
-	    			out.print("<p class=\"card-text\"><small class=\"text-muted\">"+"by "+item.getName()+" on "+dateFormat.format(item.getDate())+"</small></p>\r\n"+ "</div>");
-	    			out.print("</div>");
-	    		}else {
-	    			  out.println("<script type=\"text/javascript\">");
-	    			   out.println("alert('Incorrect values');");
-	    			   out.println("</script>");
-	    			   break;
-	    			}
-	    		}
-			out.print("</div>");
+			out.print("<div class=\"d-flex flex-row p-2 justify-content-start align-items-center \">");
+			
+			for (Message item : Db.getMessageDatabase()) {
+
+				if ((item.getName().toLowerCase().equals(name.toLowerCase()))) {
+
+					out.print(item.printName(item));
+					
+					flag = 1;
+					
+					break;
+				}
+
+				else if ((item.getName().toLowerCase().contains(name.toLowerCase()))) {
+
+					out.print(item.printName(item));
+					
+					flag = 1;
+				}
+
+				else {
+					flag = 0;
+				}
+
 			}
+
+			if (flag == 0) {
+				out.print(Message.printError());
+			}
+			
+			out.print("</div>");
+			
+		} else {
+			String date = request.getParameter("date");
+
+			out.print("<div class=\"d-flex flex-column p-5 justify-content-center align-items-center \">");
+
+			for (Message item : Db.getMessageDatabase()) {
+
+				if (dateFormat.format(item.getDate()).toString().equals(date)) {
+
+					out.print(item.printDate(item));
+
+				} else {
+
+					out.println(Message.printError());
+					break;
+				}
+			}
+			out.print("</div>");
 		}
+
 		out.print("</body>");
 		out.print("</html>");
 		out.close();
